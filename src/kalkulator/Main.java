9c78f4fb991764e37575e5d979ead66411612ba0
sanan90 <san-an@bk.ru>
2020-08-31 03:57:43 +0300

    HW

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -1,20 +1,22 @@
-package sample;
+package kalkulator;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("");
-        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.setTitle("Calculator");
+        primaryStage.setScene(new Scene(root, 300, 200));
         primaryStage.show();
-
     }
 
 
